cf7ca663ebb5374b0432c7ec692aca86c69f6ff6
Miraj Shekhda <mirajshekhda41@gmail.com>
2025-07-22 08:57:52 +0530

    Time: 26 ms (26.06%), Space: 26.1 MB (47.85%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mpp;
+
+        for(auto it : strs){
+            string word = it;
+            sort(word.begin(),word.end());
+            mpp[word].push_back(it);
+        }
+
+        vector<vector<string>> answer;
+        for(auto it : mpp){
+            answer.push_back(it.second);
+        }
+
+        return answer;
+    }
+};
