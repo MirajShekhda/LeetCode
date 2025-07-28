b61693fab4133566c01a2415bfb7db9b38ec0424
Miraj Shekhda <mirajshekhda41@gmail.com>
2025-07-28 10:43:40 +0530

    Time: 259 ms (14.13%), Space: 143.9 MB (17.27%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    int mxOr = 0;
-
-    int cntMaxOr(vector<int> nums, int idx, int currOr){
+    int countMaxOrSubsets(vector<int>& nums) {
+        int maxOr = 0;
+        for(auto& num : nums){
+            maxOr |= num;
+        }
+        return cntMaxOr(nums,maxOr,0,0);
+    }
+private:
+    int cntMaxOr(vector<int> nums, int maxOr, int idx, int currOr){
         int n = nums.size();
         if(idx == n){
-            if(currOr == mxOr){
+            if(currOr == maxOr){
                 return 1;
             }else{
                 return 0;
             }
         }
-        int take = cntMaxOr(nums, idx+1, currOr | nums[idx]);
-        int nottake = cntMaxOr(nums, idx+1, currOr);
+        if(currOr == maxOr){
+            return 1 << (n - idx);
+        }
+        int take = cntMaxOr(nums, maxOr, idx+1, currOr | nums[idx]);
+        int nottake = cntMaxOr(nums, maxOr, idx+1, currOr);
         return take + nottake;
     } 
-
-    int countMaxOrSubsets(vector<int>& nums) {
-        int mx = 0;
-        for(auto& num : nums){
-            mx |= num;
-        }
-        mxOr = mx;
-
-        return cntMaxOr(nums,0,0);
-    }
 };
